e28520a5d2845de658f6673536cf7d1e80b27fb1
WeatherAppProject <106649490+WeatherAppProject@users.noreply.github.com>
2022-06-15 09:01:31 +0200

    Update dynamic.js

--- dynamic.js
+++ dynamic.js
@@ -1,12 +1,14 @@
-			var apiUrl = 'http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=53.46919&lon=7.48232&appid=bff1c970eb02f1a1ac915c9bf9c6ce9b';
-			fetch(apiUrl).then(response => {
-				return response.json();
-			}).then(data => {
+function myFunction() {
+	var apiUrl = 'http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=53.46919&lon=7.48232&appid=bff1c970eb02f1a1ac915c9bf9c6ce9b';
+	fetch(apiUrl).then(response => {
+		return response.json();
+	}).then(data => {
 	
-			document.getElementById("json").textContent = JSON.stringify(data, undefined, 2);
+	document.getElementById("json").textContent = JSON.stringify(data, undefined, 2);
 	
-			}).catch(err => {
+	}).catch(err => {
 	
-			alert('Sussy Error Lol');
+	alert('Sussy Error Lol');
 
       });
+}
