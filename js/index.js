5d8fc8d012055ff1d8d3260e68a1d5965a19672b
WeatherAppProject <106649490+WeatherAppProject@users.noreply.github.com>
2022-06-29 09:08:27 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 
+function openGym() {
+	window.open('http://www.ulricianum-aurich.net/', '_blank')
+}
+
 function GetAPI() {
 	var apiUrl = 'http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=53.46919&lon=7.48232&appid=bff1c970eb02f1a1ac915c9bf9c6ce9b';
 	fetch(apiUrl).then(response => {
