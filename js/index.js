78d1caac9558cb56eedc74570c1f89028bb03312
WeatherAppProject <106649490+WeatherAppProject@users.noreply.github.com>
2022-06-29 09:01:40 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -13,14 +13,23 @@ function GetAPI() {
 		for (var i = 0; i <= Object.keys(obj.list).length - 1; i++) {
 			var luft = new Array();
 			var zeit = new Array();
-			
+			var s = new Array();
+
 			delete obj.list[i].components;
 
-			luft[i] = obj.list[i].main.aqi;
+			luft[i] = 'Luftstatus: ' + obj.list[i].main.aqi;
 			zeit[i] = obj.list[i].dt;
 
 			console.log(luft[i]);
-			console.log(zeit[i]);
+			s[i] = new Date(zeit[i] * 1000).toLocaleDateString("de-DE");
+			const d = new Date();
+			var hour = d.getHours() + i + 1;
+
+			while (hour > 23) {
+				hour = hour - 24;
+			}
+			s[i] = s[i] + ' ' + hour + ' Uhr';
+			console.log(s[i]);
 		}
 
 		document.getElementById("json").textContent = pureJson;
